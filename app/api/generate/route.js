40eb4e66e4fc6ef8f076fa27791b33c8f6161e9b
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-08-18 13:47:28 -0400

    restarted the repo

---
+++ app/api/generate/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import OpenAI from 'openai'
+
+const systemPrompt = `
+You are a flashcard creator. 
+Your task is to generate concise, clear, and informative flashcards on a given topic. 
+Each flashcard should contain a question on one side and a brief, accurate answer on the other. 
+Ensure that the content is educational and easy to understand. 
+If applicable, include examples, definitions, or key points to enhance learning. 
+Focus on creating flashcards that facilitate quick recall and review of the material.
+
+Remember, the goal is to facilitate effective learnign and retention of information through these flashcards.
+
+Return in the following json format
+
+{
+    "flashcards":[
+        {
+            "front": str,
+            "back": str
+        }
+    ]
+}
+`;
+
+export async function POST(req) {
+    const openai = OpenAI()
+    const data = await req.text()
+
+    const completion = await openai.chat.completion.create({
+        messages: [
+            {role: 'system', content: systemPrompt},
+            {role: 'user', content: data},
+        ],
+        model: "gpt-4o",
+        response_format: {type: 'json_object'},
+    })
+
+    const flashcards = JSON.parse(completion.choices[0].message.content)
+
+    return NextResponse.json(flashcards.flashcards)
+}
+
+
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_krFU24NhnoMmWZgfk2FdLB4bogPoEg8",
+  authDomain: "flashcardsaas-46efd.firebaseapp.com",
+  projectId: "flashcardsaas-46efd",
+  storageBucket: "flashcardsaas-46efd.appspot.com",
+  messagingSenderId: "588305515450",
+  appId: "1:588305515450:web:3aeaefd59b2ef1cead5383",
+  measurementId: "G-JTMJR0WCR3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
