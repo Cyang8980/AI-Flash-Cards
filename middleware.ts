145e2b405182bd9aa914c0ea42f1f6e7cd8089d1
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-08-18 22:46:00 -0400

    undo changes made to middleware to test

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
+export default clerkMiddleware();
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
@@ -9,4 +11,3 @@ export const config = {
   ],
 };
 
-export default clerkMiddleware();
